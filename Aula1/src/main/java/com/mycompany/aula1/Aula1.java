8d337e95ae21b1c61b448e6e26b194d9a12b6229
Lucas Nunes <161032233+lmnunes2002@users.noreply.github.com>
2024-07-31 11:36:07 -0300

    Add files via upload

---
+++ Aula1/src/main/java/com/mycompany/aula1/Aula1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula1;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Aula1 {
+
+    public static void main(String[] args) {
+        //Declaração de variáveis.
+        String nome = "Lucas";
+        int idade = 22;
+        double peso = 70;
+        
+        //Declaração de constante.
+        final String CPF = "123.456.789-00";
+        
+        //Exibindo resultados.
+        System.out.println("O nome do usuário é: " + nome);
+        System.out.println("A idade do usuário é: " + idade + " anos");
+        System.out.println("O peso do usuário é: " + peso + " quilos");
+        System.out.println("O cpf do usuário é: " + CPF);
+    }
+}
---
+++ Operadores/src/main/java/com/mycompany/operadores/Operadores.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operadores;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Operadores {
+
+    public static void main(String[] args) {
+        //Declaração de variáveis.
+        int a =10;
+        int b = 2;
+        
+        //Operando.
+        int somar = a + b;
+        int subtrair = a - b;
+        int multiplicar = a * b;
+        int dividir = a / b;
+        
+        //Exibindo resultados.
+        System.out.println("Somar: "+ somar);
+        System.out.println("Subtrair: " + subtrair);
+        System.out.println("Multiplicar: " + multiplicar);
+        System.out.println("Dividir: " + dividir);
+    }
+}
