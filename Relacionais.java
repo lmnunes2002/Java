25caefd8f1ace491926758f3f940afe881bcb724
Lucas Nunes <161032233+lmnunes2002@users.noreply.github.com>
2024-08-01 09:22:54 -0300

    Add files via upload

---
+++ Relacionais.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.relacionais;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Relacionais {
+
+    public static void main(String[] args) {
+        //Declaração de variáveis.
+        int a = 10;
+        int b = 2;
+        int maior;
+        
+        //Relacionais.
+        if (a > b) {
+            maior = a;
+         } else {
+            maior = b;
+         }
+        
+        //Exibindo resultados.
+        System.out.println("O maior número é: " + maior);
+    }
+}
